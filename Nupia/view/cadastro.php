aa9e999d37354de2d30a2d2d3bb2f135f0289344
Raimundo Lima <rai8991@hotmail.com>
2018-07-17 20:25:14 -0300

    w30 reload

--- Nupia/view/cadastro.php
+++ Nupia/view/cadastro.php
@@ -6,31 +6,31 @@
 <div class="center">
 <h2 >Cadastrar</h2>
 		<form method="POST" action="/cadastrando">
-          <div  class="input-field inline w30">
+          <div  class="input-field inline">
             <input required id="nome"name="nome" type="text">
             <label for="nome">Primeiro Nome</label>
             <span class="helper-text" data-error="wrong" data-success="right"></span>
           </div>
 		  <br>
-          <div class="input-field inline w30">
+          <div class="input-field inline ">
             <input required id="sobrenome"name="sobrenome" type="text">
             <label for="sobrenome">Ultimo Nome</label>
             <span class="helper-text" data-error="wrong" data-success="right"></span>
           </div>
 		  <br>
-          <div class="input-field inline w30">
+          <div class="input-field inline ">
             <input required name="email" id="email" type="email">
             <label for="email">Email</label>
             <span class="helper-text" data-error="wrong" data-success="right"></span>
           </div>
 		  <br>
-          <div class="input-field inline w30">
+          <div class="input-field inline ">
             <input required name="senha" id="senha" type="password">
             <label for="senha">Senha</label>
             <span class="helper-text" data-error="wrong" data-success="right">De 6 até 20 caracteres</span>
           </div>
 		  <br>
-          <div class="w30 input-field inline">
+          <div class=" input-field inline">
 				<select required name="instituicao" >
 				  <option value="" disabled selected>Escolha uma Instituição</option>
 				  <option value="1">IFRS-campus rio grande</option>
