ff5b453ab61ded9a0819a0660b60251b02df71c8
Raimundo Lima <rai8991@hotmail.com>
2018-07-18 06:19:58 -0300

    Buger menu

--- Nupia/view/home.php
+++ Nupia/view/home.php
@@ -3,7 +3,7 @@
 ?>
 <br><br>
 <img width="100%" height="0%" src="view/img/NupiaPessoas.jpg" >
-<div style="border: 1px solid #a8dea4;" class="container">
+<div style="border: 1px solid #a8dea4;" class="container ">
 <h2 class="" >&nbsp <b style="color: black;">Nupia</b></h2>
 <p class="flow-text">&nbsp&nbsp&nbsp&nbspO Estar atento a segurança alimentar e educar futuros profissionais e cidadãos para estímulo e realização de atividades agroprodutivas foram um impulso é a criaçao do projeto <i>Núcleo de Unidades Pedagógicas Integradoras Agroprodutivas</i>, o Nupia. Uma iniciativa do Instituto Federal do Rio Grande do Sul (IFRS), Campus Bento Gonçalves, em parceria com a Empresa Brasileira de Pesquisa Agropecuária (Embrapa), contando com o apoio da Fundação Estadual de Pesquisa Agropecuária (Fepagro).</p>
 
