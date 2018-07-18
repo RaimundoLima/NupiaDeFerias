6eb749178cbf7e7ea07b0f41f090e7775addb0dd
Raimundo Lima <rai8991@hotmail.com>
2018-07-18 08:06:57 -0300

    emailProntoParaTeste

--- Nupia/view/enviaEmail.php
+++ Nupia/view/enviaEmail.php
@@ -1,14 +1,24 @@
 <?php
-// O remetente deve ser um e-mail do seu dom�nio conforme determina a RFC 822.
+// O remetente deve ser um e-mail do seu dom�nio conforme determina a RFC 822.
 // O return-path deve ser ser o mesmo e-mail do remetente.
-$headers = "MIME-Version: 1.1\r\n";
-$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
-$headers .= "From: raimundolima8991@gmail.com\r\n"; // remetente
-$headers .= "Return-Path: raimundolima8991@gmail.com\r\n"; // return-path
-$envio = mail("destinatario@algum-email.com", "Assunto", "Texto", $headers);
- 
-if($envio)
- echo "Mensagem enviada com sucesso";
-else
- echo "A mensagem n�o pode ser enviada";
+ $arquivo;//html a ser enviado
+$emailenviar = "seuemail@seudominio.com.br";
+$destino = $email;//usuario
+$assunto = "Cadastro na plataforma";
+
+// É necessário indicar que o formato do e-mail é html
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From: $nome <$email>';
+//$headers .= "Bcc: $EmailPadrao\r\n";
+
+$enviaremail = mail($destino, $assunto, $arquivo, $headers);
+if($enviaremail){
+$mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+echo " <meta http-equiv='refresh' content='10;URL=Home'>";
+} else {
+$mgm = "ERRO AO ENVIAR E-MAIL!";
+echo "";
+}
+?>
 ?>
