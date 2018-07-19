697dd7926e98461c76be9eda3fb9d11e63c5389c
Raimundo Lima <rai8991@hotmail.com>
2018-07-19 19:08:45 -0300

    eixos Dinamicos

--- Nupia/view/eixos.php
+++ Nupia/view/eixos.php
@@ -3,14 +3,14 @@
 ?>
 <br><br><br><br>
 <div style="border: 1px solid #a8dea4;" class="container">
-<h2 class="" >&nbsp <b style="color: black;">Eixos</b></h2>
+<h2 style='padding-left: 0.5em;' > <b style="color: black;">Eixos</b></h2>
 <p style="color: black;" class="flow-text">&nbsp&nbsp&nbsp&nbspNo projeto existem existem 3 eixos <i><b>Ensino,Pesquisa,Extensão</b></i> e eles são focados em ...</p>
-<h2 class="" >&nbsp <b style="color: green;">Ensino</b></h2>
-<p style="color: black;" class="flow-text">&nbsp&nbsp&nbsp&nbspNo projeto existem existem 3 eixos <i><b>Ensino,Pesquisa,Extensão</b></i> e eles são focados em ...</p>
-<h2 class="" >&nbsp <b style="color: green;">Pesquisa</b></h2>
-<p style="color: black;" class="flow-text">&nbsp&nbsp&nbsp&nbspNo projeto existem existem 3 eixos <i><b>Ensino,Pesquisa,Extensão</b></i> e eles são focados em ...</p>
-<h2 class="" >&nbsp <b style="color: green;">Extensão</b></h2>
-<p style="color: black;" class="flow-text">&nbsp&nbsp&nbsp&nbspNo projeto existem existem 3 eixos <i><b>Ensino,Pesquisa,Extensão</b></i> e eles são focados em ...</p>
+<?php
+	foreach($eixo as $lista){
+		echo '<h2 class="" >&nbsp <b style="color: green;">'.$eixo->getNome().'</b></h2>';
+		echo '<p style="color: black;" class="flow-text">&nbsp&nbsp&nbsp&nbsp'.$eixo->getDescricao().'</p>';
+	}
+?>
 <?php
 	include("footer.php");
 ?>
